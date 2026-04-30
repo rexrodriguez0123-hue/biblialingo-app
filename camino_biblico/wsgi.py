86018377da0845ce2f26ef54dcbf0e4259a213e4
rexrodriguez0123-hue <rexrodriguez0123@gmail.com>
2026-04-30 12:53:07 -0500

    fix: corregir ruta del modulo de settings

--- camino_biblico/wsgi.py
+++ camino_biblico/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for Camino Biblico project.
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Camino Biblico.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'camino_biblico.settings.prod')
 
 application = get_wsgi_application()
 
